2a56c39a9b6150af269efbda489ef280971229b1
Seidy <seidyalanis@gmail.com>
2026-02-22 21:56:13 -0600

    Método para obtener los datos del formulario.

--- js/vehiculo/editarVehiculo.js
+++ js/vehiculo/editarVehiculo.js
@@ -1,3 +1,13 @@
 const apiBaseUrl = 'http://localhost:3001';
 
 
+function obtenerDatosFormulario() {
+    return {
+        marca: document.getElementById('marca').value.trim(),
+        modelo: document.getElementById('modelo').value.trim(),
+        anno: parseInt(document.getElementById('anno').value),
+        precio: parseFloat(document.getElementById('precio').value)
+    };
+}
+
+
