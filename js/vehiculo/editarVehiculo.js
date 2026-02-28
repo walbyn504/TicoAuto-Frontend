8e614e9f15f73c6d863098b9a6b400ed5d326bf0
Seidy <seidyalanis@gmail.com>
2026-02-28 17:15:23 -0600

    Modificación de método para separar responsabilidades.

--- js/vehiculo/editarVehiculo.js
+++ js/vehiculo/editarVehiculo.js
@@ -1,38 +1,6 @@
 const apiBaseUrl = 'http://localhost:3001';
 
 
-function obtenerDatosFormulario() {
-    return {
-        marca: document.getElementById('marca').value.trim(),
-        modelo: document.getElementById('modelo').value.trim(),
-        anno: parseInt(document.getElementById('anno').value),
-        precio: parseFloat(document.getElementById('precio').value)
-    };
-}
-
-
-function validarVehiculo(vehiculo) {
-
-    if (vehiculo.marca === '') {
-        return "La marca es obligatoria";
-    }
-
-    if (vehiculo.modelo === '') {
-        return "El modelo es obligatorio";
-    }
-
-    if (isNaN(vehiculo.anno) || vehiculo.anno <= 0) {
-        return "El año debe ser un número valido mayor a 0";
-    }
-
-    if (isNaN(vehiculo.precio) || vehiculo.precio <= 0) {
-        return "El precio debe ser un número valido mayor a 0";
-    }
-
-    return null; // No hay errores
-}
-
-
 async function editarVehiculo(id) {
 
     const vehiculo = obtenerDatosFormulario();
