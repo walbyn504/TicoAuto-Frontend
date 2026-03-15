c04a6b6a27a0850901a9c680273a11eca79d3b4d
walbyn <walbyngonzalez@gmail.com>
2026-03-14 19:18:47 -0600

    Se crea el archivo de respuestas

---
+++ js/mensajes/respuestas.js
@@ -0,0 +1,38 @@
+async function enviarRespuesta(preguntaId) {
+
+    const textarea = document.getElementById("textoPregunta");
+    const texto = textarea.value.trim();
+
+    if (!texto) {
+        alert("Debes escribir una respuesta.");
+        return;
+    }
+
+    try {
+
+        const response = await fetch(`${apiBaseUrl}/api/pregunta/${preguntaId}/respuesta`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": `Bearer ${token}`
+            },
+            body: JSON.stringify({
+                respuesta: texto
+            })
+        });
+
+        const data = await response.json();
+
+        if (!response.ok) {
+            alert(data.mensaje);
+            return;
+        }
+
+        textarea.value = "";
+        await cargarConversaciones();
+
+    } catch (error) {
+        console.error(error);
+        alert("Error al enviar respuesta.");
+    }
+}
